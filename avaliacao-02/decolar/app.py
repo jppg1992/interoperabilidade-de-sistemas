ed3c13833ff86b913d0a4c79364f0ac3536b11a5
João Paulo Godinho <j_paulo_g@hotmail.com>
2024-07-18 01:25:19 -0300

    finalizando atividade

--- avaliacao-02/decolar/app.py
+++ avaliacao-02/decolar/app.py
@@ -2,6 +2,10 @@ from flask import Flask, json, render_template, request, redirect, jsonify
 import sqlite3
 import requests
 
+URL_BASE_GOL ="http://localhost:8081" 
+URL_BASE_LATAM ="http://localhost:8082" 
+
+
 app = Flask(__name__, template_folder='.')
 
 @app.route('/listar', methods=['GET','POST'])
@@ -10,12 +14,19 @@ def listar ():
 		data   = request.values.get('data')
 		print(data)
 		txt = json.dumps({"data":data}) 
-		respostaGol = requests.post(url="http://localhost:8001/servico-voos.php",data=txt)
+		respostaGol = requests.post(url=URL_BASE_GOL+"/servico-voos.php",data=txt)
 		txt = respostaGol.content
 		 
-		resultado = json.loads(txt)
-		print(resultado[0])
-		return render_template('listar.html',resultado=resultado)
+		listaGol = json.loads(txt)
+		print(listaGol)	
+		txt = json.dumps({"data":data}) 
+		respLatam = requests.post(url=URL_BASE_LATAM+"/servico-voos.php",data=txt)
+		txt = respLatam.content
+		listaLatam = json.loads(txt)
+
+		
+		print(listaLatam)
+		return render_template('listar.html',listaGol=listaGol,listaLatam=listaLatam)
 	
 	return render_template('listar.html')
 
@@ -23,12 +34,29 @@ def listar ():
 def comprar ():
 	id = request.values.get('id')
 	voo = request.values.get('voo')
-	return render_template('comprar.html')
+	return render_template('comprar.html',resultado={'id':id,'voo':voo})
 
 @app.route('/confirmar', methods=['GET', 'POST'])
 def confirmar ():
-	return render_template('confirmar.html')
+	if request.method == 'POST':
+		cpf   = request.values.get('cpf')
+		nome   = request.values.get('nome')
+		id   = request.values.get('id')
+		voo   = request.values.get('voo')
+		print(cpf,nome,id,voo)
 
+	
+		if "GOL" in voo:
+			urlTxt = URL_BASE_GOL+"/servico-compra.php"
+		else:
+			urlTxt = URL_BASE_LATAM+"/servico-compra.php"
+		txt = json.dumps({"id":id,"cpf":cpf,"nome":nome}) 
+		resp = requests.post(url=urlTxt,data=txt)
+		txt = resp.content
+	 
+		resultado = json.loads(txt)
+		return render_template('confirmar.html',resultado=resultado)
+	return render_template('confirmar.html')
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	return redirect('/listar')
